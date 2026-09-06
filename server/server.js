7137cb2c491a3c4b5c5937d7e11868b685dcd914
Arushi Mishra <arushimishra1706@gmail.com>
2026-09-06 23:00:16 +0530

    Update server for deployment

--- server/server.js
+++ server/server.js
@@ -90,4 +90,4 @@ app.use((err, _req, res, _next) => {
   res.status(500).json({ error: 'Internal server error' });
 });
 
-app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Backend running on port ${PORT}`));
